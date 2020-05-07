8d68cf662da6fc76c2d0c0cbc0d11cba62d1ac68
tsthght <tsthght@163.com>
2020-05-07 17:41:26 +0800

    fix error

--- drainer/sync/mafka.go
+++ drainer/sync/mafka.go
@@ -47,7 +47,7 @@ func NewMafkaSyncer (
 	}
 
 	ret := C.InitProducerOnce(C.CString(cfgFile))
-	if len(C.GoString(ret)) == 0 {
+	if len(C.GoString(ret)) > 0 {
 		return nil, errors.New("init producer error")
 	}
 
