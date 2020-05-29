b0e98c9ddb5242e0177d40cb21682932e3295c6a
tsthght <tsthght@163.com>
2020-05-29 20:50:03 +0800

    add plg

---
+++ drainer/syncplg/syncerdemo/syncerdemo.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/pingcap/tidb-binlog/drainer/loopbacksync"
+	"github.com/pingcap/tidb-binlog/drainer/relay"
+	"github.com/pingcap/tidb-binlog/drainer/sync"
+	"github.com/pingcap/tidb-binlog/drainer/translator"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+//NewSyncerPlugin return A syncer instance which implemented interface of sync.Syncer
+func NewSyncerPlugin (
+	cfg *sync.DBConfig,
+	cfgFile string,
+	tableInfoGetter translator.TableInfoGetter,
+	worker int,
+	batchSize int,
+	queryHistogramVec *prometheus.HistogramVec,
+	sqlMode *string,
+	destDBType string,
+	relayer relay.Relayer,
+	info *loopbacksync.LoopBackSync,
+	enableDispatch bool,
+	enableCausility bool,
+) (dsyncer sync.Syncer, err error) {
+	return sync.NewSyncerDemo(cfg, cfgFile, tableInfoGetter, worker, batchSize, queryHistogramVec, sqlMode,
+		destDBType, relayer, info, enableDispatch, enableCausility)
+}
